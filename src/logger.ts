931f47a6a6abd38dd225dd075badf8da17113e09
pogg0 <poggo201103@naver.com>
2021-06-15 18:15:30 +0900

    first commit

---
+++ src/logger.ts
@@ -0,0 +1,13 @@
+// 복수 객체 내보내기
+
+export function debug(msg : any) {
+  console.debug(msg);
+}
+
+export function log(msg : any) {
+  console.log(msg);
+}
+
+export function error(msg : any) {
+  console.error(msg);
+}
